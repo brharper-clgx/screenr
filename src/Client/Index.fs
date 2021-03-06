b43702a13a36e5e1bdac54e09b0d81881eae9497
Bryan Harper <brharper@corelogic.com>
2021-03-06 09:12:43 -0600

    Initialize project'

---
+++ src/Client/Index.fs
@@ -0,0 +1,125 @@
+module Index
+
+open Elmish
+open Fable.Remoting.Client
+open Shared
+
+type State =
+    { Todos: Todo list
+      Input: string }
+
+type Msg =
+    | GotTodos of Todo list
+    | SetInput of string
+    | AddTodo
+    | AddedTodo of Todo
+
+let todosApi =
+    Remoting.createApi()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.buildProxy<ITodosApi>
+
+let init(): State * Cmd<Msg> =
+    let model =
+        { Todos = []
+          Input = "" }
+    let cmd = Cmd.OfAsync.perform todosApi.getTodos () GotTodos
+    model, cmd
+
+let update (msg: Msg) (state: State): State * Cmd<Msg> =
+    match msg with
+    | GotTodos todos ->
+        { state with Todos = todos }, Cmd.none
+    | SetInput value ->
+        { state with Input = value }, Cmd.none
+    | AddTodo ->
+        let todo = Todo.create state.Input
+        let cmd = Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
+        { state with Input = "" }, cmd
+    | AddedTodo todo ->
+        { state with Todos = state.Todos @ [ todo ] }, Cmd.none
+
+open Feliz
+open Feliz.Bulma
+
+let navBrand =
+    Bulma.navbarBrand.div [
+        Bulma.navbarItem.a [
+            prop.href "https://safe-stack.github.io/"
+            navbarItem.isActive
+            prop.children [
+                Html.img [
+                    prop.src "/favicon.png"
+                    prop.alt "Logo"
+                ]
+            ]
+        ]
+    ]
+
+let containerBox (model : State) (dispatch : Msg -> unit) =
+    Bulma.box [
+        Bulma.content [
+            Html.ol [
+                for todo in model.Todos do
+                    Html.li [ prop.text todo.Description ]
+            ]
+        ]
+        Bulma.field.div [
+            field.isGrouped
+            prop.children [
+                Bulma.control.p [
+                    control.isExpanded
+                    prop.children [
+                        Bulma.input.text [
+                            prop.value model.Input
+                            prop.placeholder "What needs to be done?"
+                            prop.onChange (fun x -> SetInput x |> dispatch)
+                        ]
+                    ]
+                ]
+                Bulma.control.p [
+                    Bulma.button.a [
+                        color.isPrimary
+                        prop.disabled (Todo.isValid model.Input |> not)
+                        prop.onClick (fun _ -> dispatch AddTodo)
+                        prop.text "Add"
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let render (state : State) (dispatch : Msg -> unit) =
+    Bulma.hero [
+        hero.isFullHeight
+        color.isPrimary
+        prop.style [
+            style.backgroundSize "cover"
+            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
+            style.backgroundPosition "no-repeat center center fixed"
+        ]
+        prop.children [
+            Bulma.heroHead [
+                Bulma.navbar [
+                    Bulma.container [
+                        navBrand
+                    ]
+                ]
+            ]
+            Bulma.heroBody [
+                Bulma.container [
+                    Bulma.column [
+                        column.is6
+                        column.isOffset3
+                        prop.children [
+                            Bulma.title [
+                                text.hasTextCentered
+                                prop.text "Screenr"
+                            ]
+                            containerBox state dispatch
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
